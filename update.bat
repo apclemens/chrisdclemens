697bd67c13baad6797cb9d7b638f12fd8846c894
Andrew Clemens <apclemens1@gmail.com>
2023-12-29 18:37:26 -0500

    add update script

--- update.bat
+++ update.bat
@@ -1,2 +1,6 @@
-C:\Program Files\Git\cmd
-git pull origin master ; python .\main.py ; git add . ; git commit -m "update content" ; git push origin master
+set PATH=%PATH%;C:\Program Files\Git\cmd
+git pull origin master
+python .\main.py
+git add .
+git commit -m "update content"
+git push origin master
